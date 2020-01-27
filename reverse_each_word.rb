0cb08f51ea027072e0298678e43d48c12f125f10
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-27 19:03:38 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -10,13 +10,4 @@ return reversed.join(" ")
 end
 
 
-sentence=[]
-def reverse_each_word(sentence)
-  words = sentence.gsub(/\s+/m, ' ').strip.split(" ")
-  words.each do |word|
-  word.reverse
-end
-return sentence.join(" ")
-
-end
 

aedf83c0e13489be972f4c4d9ef4010b712099fd
ANTON ZAHARIA <zaharia_anton@yahoo.com>
2020-01-27 19:02:00 +0000

    Automatically backed up by Learn

--- reverse_each_word.rb
+++ reverse_each_word.rb
@@ -1,9 +1,9 @@
 sentence=[]
 def reverse_each_word(sentence)
-reversed = []
+
   words = sentence.gsub(/\s+/m, ' ').strip.split(" ")
   words.each do |word|
-  reversed << word.reverse
+   word.reverse
 end
 return reversed.join(" ")
 
